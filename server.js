845e1074b39a511a28dbd7f133b06c9e9392d470
Richard Alhama <richard.alhama@gmail.com>
2024-06-22 05:02:12 +0000

    update node modules

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 const DPSCitationsRoutes = require('./routes/DPSCitationRoutes');
-
 require('dotenv').config();
 
 const app = express();
-app.use(cors())
-
-const port = process.env.PORT || (process.env.NODE_ENV === 'production' ? 3081 : 3002);;
-
+app.use(cors());
 app.use(bodyParser.json());
+
+const port = process.env.PORT || (process.env.NODE_ENV === 'production' ? 3081 : 3002);
 
 let mongoUri;
 
@@ -24,17 +22,21 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 })
-.then(()=>{
+.then(() => {
   console.log(process.env.NODE_ENV);
   console.log('Connected to MongoDB');
 })
-.catch((err)=>{
-  console.error('Error connecting to MongoDB:', err.message)
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err.message);
 });
 
 app.use('/users', userRoutes);
-app.use('/dpscitations',  DPSCitationsRoutes)
+app.use('/dpscitations', DPSCitationsRoutes);
 
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
