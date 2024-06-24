597b909b7602931f4b447d26a01b04a5d4580ac6
Richard Alhama <richard.alhama@gmail.com>
2024-06-24 06:45:10 +0000

    use https

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
+const fs = require('fs');
+const https = require('https');
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 const DPSCitationsRoutes = require('./routes/DPSCitationRoutes');
+
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
 
 const port = process.env.PORT || (process.env.NODE_ENV === 'production' ? 3081 : 3002);
+app.use(bodyParser.json());
 
 let mongoUri;
 
@@ -32,6 +35,24 @@ mongoose.connect(mongoUri, {
 
 app.use('/users', userRoutes);
 app.use('/dpscitations', DPSCitationsRoutes);
+
+//Non https
+//app.listen(port, () => {
+//  console.log(`Server is up and running on port ${port}`);
+//});
+
+//https
+// Load TLS certificate and key
+const options = {
+  // key: fs.readFileSync('/etc/letsencrypt/live/your_domain/privkey.pem'),
+  // cert: fs.readFileSync('/etc/letsencrypt/live/your_domain/fullchain.pem')
+  cert: fs.readFileSync('/node-tls/fullchain.pem'),
+  key: fs.readFileSync('/node-tls/privkey.pem')
+};
+
+https.createServer(options, app).listen(port, () => {
+  console.log(`Server is up and running on port ${port} with TLS`);
+});
 
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
