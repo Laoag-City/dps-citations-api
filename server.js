2e30884a3be2d72f87025f93048c83b22a3ffa94
Richard Alhama <richard.alhama@gmail.com>
2024-06-25 01:22:58 +0000

    improved dev and prod logic with conflict resolution

--- server.js
+++ server.js
@@ -10,46 +10,42 @@ const DPSCitationsRoutes = require('./routes/DPSCitationRoutes');
 require('dotenv').config();
 
 const app = express();
- 
+app.use(cors());
 
 const port = process.env.PORT || (process.env.NODE_ENV === 'production' ? 3081 : 3002);
 app.use(bodyParser.json());
 
-let mongoUri;
-
-// Choose appropriate MongoDB URI based on environment
-if (process.env.NODE_ENV === 'production') {
-  mongoUri = process.env.MONGO_URI_PROD;
-} else {
-  mongoUri = process.env.MONGO_URI_DEV;
-}
-
-mongoose.connect(mongoUri, {
-}).then(()=>{
-  console.log(process.env.NODE_ENV);
-  console.log('Connected to MongoDB');
-})
-.catch((err) => {
-  console.error('Error connecting to MongoDB:', err.message);
-});
-
 app.use('/users', userRoutes);
 app.use('/dpscitations', DPSCitationsRoutes);
 
-//Non https
-//app.listen(port, () => {
-//  console.log(`Server is up and running on port ${port}`);
-//});
+// Determine MongoDB URI and server setup based on environment
+if (process.env.NODE_ENV === 'production') {
+  mongoUri = process.env.MONGO_URI_PROD;
+  // Load TLS certificate and key
+  const options = {
+    cert: fs.readFileSync('/node-tls/fullchain.pem'),
+    key: fs.readFileSync('/node-tls/privkey.pem')
+  };
 
-//https
-// Load TLS certificate and key
-const options = {
-  // key: fs.readFileSync('/etc/letsencrypt/live/your_domain/privkey.pem'),
-  // cert: fs.readFileSync('/etc/letsencrypt/live/your_domain/fullchain.pem')
-  cert: fs.readFileSync('/node-tls/fullchain.pem'),
-  key: fs.readFileSync('/node-tls/privkey.pem')
-};
+  mongoose.connect(mongoUri, {}).then(() => {
+    console.log('Production environment');
+    console.log('Connected to MongoDB', mongoUri);
+    https.createServer(options, app).listen(port, () => {
+      console.log(`Server is up and running on port ${port} with TLS`);
+    });
+  }).catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+  });
+} else {
+  mongoUri = process.env.MONGO_URI_DEV;
 
-https.createServer(options, app).listen(port, () => {
-  console.log(`Server is up and running on port ${port} with TLS`);
-});
+  mongoose.connect(mongoUri, {}).then(() => {
+    console.log('Development environment');
+    console.log('Connected to MongoDB', mongoUri);
+    app.listen(port, () => {
+      console.log(`Server is up and running on port ${port}`);
+    });
+  }).catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+  });
+}
