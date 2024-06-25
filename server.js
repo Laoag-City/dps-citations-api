a03ac067d3aa63aadd4198407660e3871ad18ede
Richard Alhama <richard.alhama@gmail.com>
2024-06-25 01:36:43 +0000

    added logger and config utilities

--- server.js
+++ server.js
@@ -6,46 +6,30 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 const DPSCitationsRoutes = require('./routes/DPSCitationRoutes');
-
-require('dotenv').config();
+const config = require('./config');
+const logger = require('./utils/logger');
 
 const app = express();
 app.use(cors());
-
-const port = process.env.PORT || (process.env.NODE_ENV === 'production' ? 3081 : 3002);
 app.use(bodyParser.json());
 
 app.use('/users', userRoutes);
 app.use('/dpscitations', DPSCitationsRoutes);
 
-// Determine MongoDB URI and server setup based on environment
-if (process.env.NODE_ENV === 'production') {
-  mongoUri = process.env.MONGO_URI_PROD;
-  // Load TLS certificate and key
-  const options = {
-    cert: fs.readFileSync('/node-tls/fullchain.pem'),
-    key: fs.readFileSync('/node-tls/privkey.pem')
-  };
+// Connect to MongoDB
+mongoose.connect(config.mongoUri, {}).then(() => {
+  logger.info(`${process.env.NODE_ENV} environment`);
+  logger.info('Connected to MongoDB', { mongoUri: config.mongoUri });
 
-  mongoose.connect(mongoUri, {}).then(() => {
-    console.log('Production environment');
-    console.log('Connected to MongoDB', mongoUri);
-    https.createServer(options, app).listen(port, () => {
-      console.log(`Server is up and running on port ${port} with TLS`);
+  if (process.env.NODE_ENV === 'production') {
+    https.createServer(config.tlsOptions, app).listen(config.port, () => {
+      logger.info(`Server is up and running on port ${config.port} with TLS`);
     });
-  }).catch((err) => {
-    console.error('Error connecting to MongoDB:', err.message);
-  });
-} else {
-  mongoUri = process.env.MONGO_URI_DEV;
-
-  mongoose.connect(mongoUri, {}).then(() => {
-    console.log('Development environment');
-    console.log('Connected to MongoDB', mongoUri);
-    app.listen(port, () => {
-      console.log(`Server is up and running on port ${port}`);
+  } else {
+    app.listen(config.port, () => {
+      logger.info(`Server is up and running on port ${config.port}`);
     });
-  }).catch((err) => {
-    console.error('Error connecting to MongoDB:', err.message);
-  });
-}
+  }
+}).catch((err) => {
+  logger.error('Error connecting to MongoDB:', { message: err.message, stack: err.stack });
+});
