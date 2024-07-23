f3dde485cffb125cf360ba2ba26c73894ed37756
Richard Alhama <richard.alhama@gmail.com>
2024-07-23 06:33:02 +0000

    added search to get citation controller

--- controllers/DPSCitationController.js
+++ controllers/DPSCitationController.js
@@ -98,7 +98,8 @@ exports.createDPSCitation = async (req, res) => {
   }
 };
 
-exports.getDPSCitations = async (req, res) => {
+{/* old code (no search) 
+  exports.getDPSCitations = async (req, res) => {
   try {
     // Get page and limit from query parameters, with default values
     const page = parseInt(req.query.page) || 1;
@@ -110,6 +111,47 @@ exports.getDPSCitations = async (req, res) => {
 
     // Fetch the documents with pagination and sorting by dateApprehended in descending order
     const dpsCitations = await DPSCitation.find({})
+      .sort({ dateApprehended: -1 })
+      .skip(skip)
+      .limit(limit);
+
+    // Send the response with paginated results and metadata
+    res.status(200).json({
+      totalDocuments,
+      totalPages: Math.ceil(totalDocuments / limit),
+      currentPage: page,
+      pageSize: limit,
+      dpsCitations,
+    });
+  } catch (error) {
+    console.error('Failed to fetch DPS Citations:', error);
+    res.status(500).json({ error: 'Failed to fetch DPS Citations' });
+  }
+};
+*/}
+exports.getDPSCitations = async (req, res) => {
+  try {
+    // Get page and limit from query parameters, with default values
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    const searchQuery = req.query.search || '';
+
+    // Build the query object for the search functionality
+    const query = {
+      $or: [
+        { firstName: { $regex: searchQuery, $options: 'i' } },
+        { lastName: { $regex: searchQuery, $options: 'i' } },
+        { licenseNumber: { $regex: searchQuery, $options: 'i' } },
+        { plateNumber: { $regex: searchQuery, $options: 'i' } },
+      ],
+    };
+
+    // Fetch the total count of documents based on the search query
+    const totalDocuments = await DPSCitation.countDocuments(query);
+
+    // Fetch the documents with pagination and sorting by dateApprehended in descending order
+    const dpsCitations = await DPSCitation.find(query)
       .sort({ dateApprehended: -1 })
       .skip(skip)
       .limit(limit);
